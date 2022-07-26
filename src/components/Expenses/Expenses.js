462563de4a56d0cb760b7f47b27b284dff780ecc
TTRIFIC <beckerman.m@sbcglobal.net>
2022-07-25 19:28:56 -0500

    added comments and ability to add items to list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,7 +9,11 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+// *ABOVE* This is responsible for making the filter interactive. Since it is an interactive component, you want to use useState 
+// to be able to save what the input is for the drop down. 
 
+// *BELOW* The props.items.map portion is responsible for adding new dynamically rendered components. When a new expense is added
+// that function adds a new component to the code. This will be good to know for future uses
   return (
     <div>
       <Card className="expenses">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -10,7 +10,9 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
-
+// *BELOW* The onSaveExpenseData={saveExpenseDataHandler} is part of "Lifting the state up" through child and parent components.
+// The basic structure is this. In the child component, props.onAddExpense(expensedata), the "onAddExpense" should be the
+// same as in the parent components attribute. This connects the two components and creates the ability to pass data through
   return (
     <div className="new-expense">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
